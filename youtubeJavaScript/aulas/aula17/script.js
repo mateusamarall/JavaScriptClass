4a52e128546bf6745e0917a48255d863df979694
mateusAmaral <mateus.amaral018@gmail.com>
2020-03-02 14:34:45 -0300

    Delete script.js

--- youtubeJavaScript/aulas/aula17/script.js
+++
@@ -1,6 +0,0 @@
-let nome = document.getElementById("nome");
-let confirmanome = document.getElementById("confirmanome");
-//evento para quando sair do elemento executar uma ação
-nome.addEventListener("blur", () => {
-  confirmanome.value = nome.value;
-});
